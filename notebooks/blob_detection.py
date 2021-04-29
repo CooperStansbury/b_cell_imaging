e0247c29cecccb2a62acf6669992c04d0ae780c5
Cooper Stansbury <cstansbu@umich.edu>
2021-04-29 13:30:58 -0400

    update init

--- notebooks/blob_detection.py
+++ notebooks/blob_detection.py
@@ -10,18 +10,45 @@ from skimage.color import rgb2gray
 from skimage.feature import blob_dog, blob_log, blob_doh
 from skimage.morphology import reconstruction
 from skimage import color, morphology
+from skimage import exposure
 
 import matplotlib.pyplot as plt
 import matplotlib
 import seaborn as sns
 
 
-@numba.jit
-def preprocess_image(image):
+def preprocess_image(image_in, channel):
     """A function to perform a few preprocessing steps before 
-    running blob detection"""
+    running blob detection
+    
+    args:
+        : image_in (nd.array): raw image for processing
+        : channel (str): for channel specific parameters
+
+    return:
+        : img (nd.array): a processed image
+    """
+
+    params = {
+        'Ch2-T1' : {'gain' : 0.99},
+        'ChS2-T2' : {'gain' : 2.7},
+        'Ch1-T4' : {'gain' : 1.1},
+        'ChS1-T3' : {'gain' : 0.8},
+    }
+
+    channel_params = params[channel]
+
     # and convert to normalize greyscale
-    image_gray = rgb2gray(image) # convert image to grey scale
+    image = rgb2gray(image_in) # convert image to grey scale
+
+    # Logarithmic  pixel intensity correction
+    image = exposure.adjust_log(image, channel_params['gain'])
+
+    # dialate the image
+    seed = np.copy(image)
+    seed[1:-1, 1:-1] = image.min()
+    mask = image
+    image = image - reconstruction(seed, mask, method='dilation')
 
     # # use hard threshold
     # u, s, vt = np.linalg.svd(image_gray)
@@ -31,30 +58,32 @@ def preprocess_image(image):
     # image = np.dot(u[:,0:k] * s[0:k,], vt[0:k,])
 
     # top hatting small objects
-    selem =  morphology.disk(1)
+    selem = morphology.disk(1)
     res = morphology.white_tophat(image, selem)
     image = image - res
 
-    # dialate the image
-    seed = np.copy(image)
-    seed[1:-1, 1:-1] = image.min()
-    mask = image
-    image = image - reconstruction(seed, mask, method='dilation')
 
     return image
     
 
-def get_LoG_blobs(image, params):
+def get_LoG_blobs(image, channel):
     """A function run blob detection on an image.
     
     args:
         : image (np.array): an input image, assumed 2D
-        : params (dict): parameters for https://scikit-image.org/docs/dev/api/skimage.feature.html#skimage.feature.blob_log
+        : channel (str): parameters for https://scikit-image.org/docs/dev/api/skimage.feature.html#skimage.feature.blob_log
     
     returns:
         : blobs (np.array): first two columns are center coords, 3rd is radius 
     """
-    blobs_log = blob_log(image, **params)
+    params = {
+        'Ch2-T1' : {'min_sigma' : 5, 'max_sigma' : 20, 'num_sigma' : 3, 'threshold' : 0.1},
+        'ChS2-T2' : {'min_sigma' : 5, 'max_sigma' : 20, 'num_sigma' : 5, 'threshold' : 0.09},
+        'Ch1-T4' : {'min_sigma' : 5, 'max_sigma' : 20, 'num_sigma' : 3, 'threshold' : 0.12},
+        'ChS1-T3' : {'min_sigma' : 8, 'max_sigma' : 20, 'num_sigma' : 3, 'threshold' : 0.17},
+    }
+    channel_params = params[channel]
+    blobs_log = blob_log(image, **channel_params)
 
     # compute the radii
     blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
---
+++ notebooks/utils.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import numpy as np
+from skimage import io
+
+
+class FileLoader():
+    """ Utility class to load images into a useable structure """
+
+    def parse_filename(self, filename):
+        return {
+            'filename' : filename,
+            'group' : filename.split(" ")[0],
+            'day' : int(filename.split("Day ")[1].split("_")[0]),
+            'channel' : filename.split("_")[1].split(".")[0],
+            'channel_color' : self.channel_colors[filename.split("_")[1].split(".")[0]]
+        }
+
+    def load_imgs_from_dir(self):
+        new_rows = []
+        for f in os.listdir(self.path):
+            file_metadata = self.parse_filename(f)
+
+            full_path = f"{self.path}{f}"
+            file_metadata['image'] = io.imread(full_path, as_gray=True)
+            new_rows.append(file_metadata)
+
+        return pd.DataFrame(new_rows)
+
+    def __init__(self, dir_path):
+
+        self.channel_colors = {
+            'Ch2-T1' : 'red', 
+            'ChS2-T2' : 'yellow', 
+            'Ch1-T4' : 'cyan', 
+            'ChS1-T3' : 'blue' 
+        }
+        self.path = dir_path
+        self.df = self.load_imgs_from_dir()
